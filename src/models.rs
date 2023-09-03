9149f6d8411b7dcd3268976c1a9c75ececb9a42d
Papa Samba Diop <papesdiop@gmail.com>
2023-09-03 16:18:55 -0400

    Jira cli project step 2 development...

---
+++ src/models.rs
@@ -0,0 +1,67 @@
+use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub enum Status {
+    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub struct Epic {
+    // TODO: add fields (make sure the fields are public)
+   pub name: String,
+   pub description: String,
+   pub status: Status,
+   pub stories: Vec<u32>
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open and the stories should be an empty vector
+        Self {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![],
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
+pub struct Story {
+    // TODO: add fields (make sure the fields are public)
+    pub name: String,
+    pub description:String,
+    pub status:Status,
+
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open
+        Self{
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct DBState {
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    // TODO: add fields (make sure the fields are public)
+    pub last_item_id : u32,
+    pub epics: HashMap<u32,Epic>,
+    pub stories: HashMap<u32, Story>,
+}
+
+impl PartialEq for DBState {
+    fn eq(&self, other: &Self) -> bool {
+        self.last_item_id == other.last_item_id && self.epics == other.epics && self.stories == other.stories
+    }
+}
