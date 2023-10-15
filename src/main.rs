b6464bc791eb2c5146e0613879177815d06067a0
Papa Samba Diop <papesdiop@gmail.com>
2023-10-14 21:45:16 -0400

    Stage1 init models impl step1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+#[macro_use]
+extern crate rocket;
 
+mod cors;
+mod handlers;
+mod models;
+
+use cors::*;
+use handlers::*;
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build()
+        .mount(
+            "/",
+            routes![
+                create_question,
+                read_questions,
+                delete_question,
+                create_answer,
+                read_answers,
+                delete_answer
+            ],
+        )
+        .attach(CORS)
 }
