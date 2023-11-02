99c1f718a62e450bec47f377ca69459a5ffdddbb
Papa Samba Diop <papesdiop@gmail.com>
2023-11-01 20:18:08 -0400

    Stage3 all impl

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,9 +1,8 @@
-use rocket::{serde::json::Json, State};
+use rocket::{serde::json::Json, State, response::Responder};
 
 use crate::models::*;
 
-use crate::persistance::answers_dao::AnswersDao;
-use crate::persistance::questions_dao::QuestionsDao;
+use crate::persistance::{answers_dao::AnswersDao, questions_dao::QuestionsDao};
 
 mod handlers_inner;
 
@@ -32,31 +31,36 @@ impl From<HandlerError> for APIError {
 pub async fn create_question(
     question: Json<Question>,
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>,
-) -> Json<QuestionDetail> {
+) -> Result<Json<QuestionDetail>, APIError> {
      // TODO: update return type to be of type `Result`. Use `APIError` for the error case.
     // TODO: Replace the fake data below with a call to `handlers_inner::create_question`.
     // Return the result wrapped in JSON in the success case and an `APIError` in the error case.
     // NOTE: You can easily turn `HandlerError` into an `APIError` because of the From trait implementation above.
-    Json (
-        handlers_inner::create_question(question.into_inner(), questions_dao).await.unwrap()
-    )
+    match handlers_inner::create_question(question.into_inner(), questions_dao).await {
+        Ok(q) => Ok(Json(q)),
+        Err(e) => Err(e.into())
+    }
 }
 
 #[get("/questions")]
 pub async fn read_questions(
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>, // add the appropriate type annotation
-) -> Json<Vec<QuestionDetail>> {
-    Json (
-        handlers_inner::read_questions(questions_dao).await.unwrap()
-    )
+) -> Result<Json<Vec<QuestionDetail>>, APIError> {
+    match handlers_inner::read_questions(questions_dao).await {
+        Ok(qs) => Ok(Json(qs)),
+        Err(e) => Err(e.into())
+    }
 }
 
 #[delete("/question", data = "<question_uuid>")]
 pub async fn delete_question(
     question_uuid: Json<QuestionId>,
     questions_dao: &State<Box<dyn QuestionsDao + Sync + Send>>, // add the appropriate type annotation
-) {
-    handlers_inner::delete_question(question_uuid.into_inner(), questions_dao).await;
+) -> Result<(), APIError> {
+    match handlers_inner::delete_question(question_uuid.into_inner(), questions_dao).await{
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.into())
+    }
 }
 
 // ---- CRUD for Answers ----
@@ -65,26 +69,32 @@ pub async fn delete_question(
 pub async fn create_answer(
     answer: Json<Answer>,
     answers_dao: &State<Box<dyn AnswersDao + Send + Sync>>,
-) -> Json<AnswerDetail> {
-    Json (
-        handlers_inner::create_answer(answer.into_inner(), answers_dao).await.unwrap()
-    )
+) -> Result<Json<AnswerDetail>, APIError> {
+    match handlers_inner::create_answer(answer.into_inner(), answers_dao).await {
+        Ok(a) => Ok(Json(a)),
+        Err(e) => Err(e.into())
+    }
+    
 }
 
 #[get("/answers", data = "<question_uuid>")]
 pub async fn read_answers(
     question_uuid: Json<QuestionId>,
     answers_dao: &State<Box<dyn AnswersDao + Sync + Send>>, // add the appropriate type annotation
-) -> Json<Vec<AnswerDetail>> {
-    Json (
-        handlers_inner::read_answers(question_uuid.into_inner(), answers_dao).await.unwrap()
-    )
+) -> Result<Json<Vec<AnswerDetail>>, APIError>{
+    match handlers_inner::read_answers(question_uuid.into_inner(), answers_dao).await {
+        Ok(ax) => Ok(Json(ax)),
+        Err(e) => Err(e.into())
+    }
 }
 
 #[delete("/answer", data = "<answer_uuid>")]
 pub async fn delete_answer(
     answer_uuid: Json<AnswerId>,
     answers_dao: &State<Box<dyn AnswersDao +Sync + Send>>, // add the appropriate type annotation
-) {
-    handlers_inner::delete_answer(answer_uuid.into_inner(), answers_dao).await;
+) -> Result<(), APIError> {
+    match handlers_inner::delete_answer(answer_uuid.into_inner(), answers_dao).await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e.into())
+    }
 }
