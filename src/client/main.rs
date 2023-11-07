29b85b475e9f2c17685a7c1457f29f16843e0129
Papa Samba Diop <papesdiop@gmail.com>
2023-11-07 14:19:16 -0500

    Step4 stage1 impl client

--- src/client/main.rs
+++ src/client/main.rs
@@ -43,30 +43,30 @@ enum Commands {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // AUTH_SERVICE_IP can be set to your droplet's ip address once your app is deployed
     let auth_ip = env::var("AUTH_SERVICE_IP").unwrap_or("[::0]".to_owned());
-    let mut client: AuthClient<Channel> = todo!(); // Create new `AuthClient` instance. Propagate any errors.
+    let mut client: AuthClient<Channel> = AuthClient::connect(format!("http://{}:50051", auth_ip)).await?;// Create new `AuthClient` instance. Propagate any errors.
 
     let cli = Cli::parse();
 
     match &cli.command {
         Some(Commands::SignIn { username, password }) => {
-            let request: Request<SignInRequest> = todo!(); // Create a new `SignInRequest`.
+            let request: Request<SignInRequest> = Request::new(SignInRequest { username: username.to_owned(), password: password.to_owned() }); // Create a new `SignInRequest`.
         
             // Make a sign in request. Propagate any errors. Convert Response<SignInResponse> into SignInResponse.
-            let response: SignInResponse = todo!();
+            let response: SignInResponse = client.sign_in(request).await?.into_inner();
         
             println!("{:?}", response);
         }
         Some(Commands::SignUp { username, password }) => {
-            let request: Request<SignUpRequest> = todo!(); // Create a new `SignUpRequest`.
+            let request: Request<SignUpRequest> = Request::new(SignUpRequest { username: username.to_owned(), password: password.to_owned() }); // Create a new `SignUpRequest`.
         
-            let response: Response<SignUpResponse> = todo!(); // Make a sign up request. Propagate any errors.
+            let response: Response<SignUpResponse> = client.sign_up(request).await?; // Make a sign up request. Propagate any errors.
         
             println!("{:?}", response.into_inner());
         }
         Some(Commands::SignOut { session_token }) => {
-            let request: Request<SignOutRequest> = todo!(); // Create a new `SignOutRequest`.
+            let request: Request<SignOutRequest> = Request::new(SignOutRequest { session_token: session_token.to_owned() }); // Create a new `SignOutRequest`.
         
-            let response: Response<SignOutResponse> = todo!(); // Make a sign out request. Propagate any errors.
+            let response: Response<SignOutResponse> = client.sign_out(request).await?; // Make a sign out request. Propagate any errors.
         
             println!("{:?}", response.into_inner());
         }
